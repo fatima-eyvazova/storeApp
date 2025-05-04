481031542a0e65205ebdda219265ae1028bec99a
fatime-eyvazova12 <fatima.eyvazovaa@gmail.com>
2025-05-04 17:37:42 +0400

    added fix file js

--- src/layouts/dashboard/pages/Orders/type.js
+++
@@ -1,8 +0,0 @@
-import PropTypes from "prop-types";
-const TablePaginationActionsPropTypes = {
-    count: PropTypes.number.isRequired,
-    onPageChange: PropTypes.func.isRequired,
-    page: PropTypes.number.isRequired,
-    rowsPerPage: PropTypes.number.isRequired,
-};
-export default TablePaginationActionsPropTypes;
--- src/validationSchema/addCategory.js
+++
@@ -1,5 +0,0 @@
-import * as Yup from "yup";
-export const schema = Yup.object({
-    name: Yup.string().required("Kateqoriyanınin adı tələb olunur"),
-    image: Yup.mixed().required("Kateqoriyanınin şəkili tələb olunur"),
-}).required();
